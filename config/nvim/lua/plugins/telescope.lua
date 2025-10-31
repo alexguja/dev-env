3fbc7adc2a4c913b2a9b3a28040942249cbdce99
alexguja <al.guja@outlook.com>
2025-10-31 23:25:49 +0100

    upgrade telescope config

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,12 @@
 return {
   {
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
-  {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    tag = '0.1.8',
+    dependencies = { 
+      'nvim-lua/plenary.nvim', 
+      'nvim-telescope/telescope-ui-select.nvim',
+      {'nvim-telescope/telescope-fzf-native.nvim', build = 'make' } 
+    },
     config = function()
       require("telescope").setup({
         defaults = {
@@ -18,26 +19,31 @@ return {
             i = {
               ['<C-t>'] = require('telescope.actions.layout').toggle_preview
             },
-            -- preview = {
-            --   hide_on_startup = true
-            -- },
           },
         },
         extensions = {
           ['ui-select'] = {
             require("telescope.themes").get_dropdown {
             }
-          }
+          },
+          fzf = {}
         }
       })
 
+      require('telescope').load_extension('fzf')
+      require("telescope").load_extension("ui-select")
+
       local builtin = require("telescope.builtin")
       vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>sr', builtin.resume, {})
-      vim.keymap.set('n', '<leader>gr', builtin.lsp_references, { desc = 'Telescope LSP references' })
+      vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
+      vim.keymap.set('n', '<leader>rs', builtin.resume, {})
+      vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { desc = 'Telescope LSP references' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-      require("telescope").load_extension("ui-select")
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set('n', '<leader>en', function()
+        builtin.find_files({ cwd = vim.fn.stdpath('config') })
+      end, {})
     end
   },
 }
