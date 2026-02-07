55127f10ab0dce86c6b4612bcd643ab66976a546
alexguja <al.guja@outlook.com>
2026-02-07 17:13:13 +0100

    update theme

--- config/nvim/lua/plugins/colortheme.lua
+++ config/nvim/lua/plugins/colortheme.lua
@@ -1,9 +1,22 @@
 return {
+  -- {
+  --   "vague2k/vague.nvim",
+  --   config = function()
+  --     require("vague").setup({})
+  --     vim.cmd.colorscheme "vague"
+  --   end
+  -- },
+  -- {
+  --   "lunacookies/vim-substrata",
+  --   config = function()
+  --     vim.cmd.colorscheme "substrata"
+  --   end
+  -- },
   {
-    "vague2k/vague.nvim",
+    "rose-pine/neovim",
+    name = "rose-pine",
     config = function()
-      require("vague").setup({})
-      vim.cmd.colorscheme "vague"
+      vim.cmd.colorscheme "rose-pine"
     end
   }
 }
