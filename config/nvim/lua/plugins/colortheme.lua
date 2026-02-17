7a032fd4503f8f07f6fc6634f0f5969927e78f14
alexguja <al.guja@outlook.com>
2026-02-17 02:14:23 +0100

    new skins

--- config/nvim/lua/plugins/colortheme.lua
+++ config/nvim/lua/plugins/colortheme.lua
@@ -1,9 +1,21 @@
 return {
+  -- {
+  --   "vague2k/vague.nvim",
+  --   config = function()
+  --     require("vague").setup({})
+  --     vim.cmd.colorscheme "vague"
+  --   end
+  -- },
   {
-    "vague2k/vague.nvim",
+    "cocopon/iceberg.vim",
+    lazy = false,    -- load immediately so the colorscheme applies
+    priority = 1000, -- ensure it loads before other plugins
     config = function()
-      require("vague").setup({})
-      vim.cmd.colorscheme "vague"
-    end
-  },
+      -- Enable true colors
+      vim.opt.termguicolors = true
+
+      -- Apply the Iceberg theme
+      vim.cmd("colorscheme iceberg")
+    end,
+  }
 }
